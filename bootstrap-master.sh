11ed6c839c3392e58aad0fabe1ff2eecb04aa71a
Jason Ernst <ernstjason1@gmail.com>
2020-02-22 08:15:25 +0000

    iptables commands aren't needed

--- bootstrap-master.sh
+++ bootstrap-master.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 curl -L https://bootstrap.saltstack.com -o install_salt.sh
 sudo sh install_salt.sh -M
-iptables -A INPUT -m state --state new \ -m tcp -p tcp --dport 4505 -j ACCEPT
-iptables -A INPUT -m state --state new \ -m tcp -p tcp --dport 4506 -j ACCEPT
