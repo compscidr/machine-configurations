aefb63664901106f56eeae6280fb73d80efe734c
Jason Ernst <ernstjason1@gmail.com>
2020-02-23 17:01:09 +0000

    Added gitfs to the bootstrap of the master

--- bootstrap-salt-master.sh
+++ bootstrap-salt-master.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+
+# get salt
 sudo hostnamectl set-hostname master.jasonernst.com
 curl -L https://bootstrap.saltstack.com -o install_salt.sh
 sudo sh install_salt.sh -M
 rm install_salt.sh
+
+# get pygit so we can use the github repo for hosting
+sudo apt-get install -y python-pygit2
+
+echo 'gitfs_remotes:\n  - git@github.com:compscidr/machine-configurations.git' | tee -a /etc/salt/master.d/99-master-gitfs.conf
+sudo service salt-master restart
