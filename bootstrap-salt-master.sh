023ed32072fad66a5e450e99cdf0327a597bbbcd
Jason Ernst <ernstjason1@gmail.com>
2020-02-23 19:50:14 +0000

    Fixed newline issue in bootstrap script. Added start of top.sls

--- bootstrap-salt-master.sh
+++ bootstrap-salt-master.sh
@@ -9,5 +9,5 @@ rm install_salt.sh
 # get pygit so we can use the github repo for hosting
 sudo apt-get install -y python-pygit2
 
-echo 'gitfs_remotes:\n  - git@github.com:compscidr/machine-configurations.git' | tee -a /etc/salt/master.d/99-master-gitfs.conf
+printf 'gitfs_remotes:\n  - git@github.com:compscidr/machine-configurations.git' | tee -a /etc/salt/master.d/99-master-gitfs.conf
 sudo service salt-master restart
