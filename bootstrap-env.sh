47de3740e522c3e1e94aad15ae07ea70cc9d4839
Jason Ernst <ernstjason1@gmail.com>
2020-04-15 15:09:09 -0700

    Simplified the script - pipe curl directly to bash

--- bootstrap-env.sh
+++ bootstrap-env.sh
@@ -2,9 +2,7 @@
 
 # get salt
 sudo hostnamectl set-hostname master.jasonernst.com
-curl -L https://bootstrap.saltstack.com -o install_salt.sh
-sudo sh install_salt.sh -M -x python3 stable 2019.2
-rm install_salt.sh
+curl -s https://bootstrap.saltstack.com | sudo bash -s -- -M -x python3 stable 2019.2
 
 # get pygit so we can use the github repo for hosting
 sudo apt-get install -y python-pygit2
